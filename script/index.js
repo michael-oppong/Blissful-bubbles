0813cc1239b99ce936707a1fe15604578fa49c3b
Michael Oppong <michaelopponghero@gmail.com>
2024-09-15 18:26:52 +0000

    Changes made

--- script/index.js
+++ script/index.js
@@ -1,13 +1,16 @@
+document.title = 'Blissful Bubbles';
 const hamburger = document.querySelector('.hamburger')
-const navMenu = document.querySelector('.nav-menu')
+const navMenu = document.querySelector('ul')
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
 })
 
-document.querySelectorAll('nav-link').forEach(n =>n.addEventListener('click',()=>{
+document.querySelectorAll('a').forEach(n =>n.addEventListener('click',()=>{
     hamburger.classList.remove('active')
     navMenu.classList.remove('active')
 }))
+
+document.title = "Blissful Bubbles";
 
